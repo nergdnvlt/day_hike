94eaead8f771101d3eed7de95de71216e8f9bfef
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-13 10:56:50 -0600

    Just finished user stories.

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,4 +1,8 @@
 class Trail < ApplicationRecord
   has_many :trip_trails, dependent: :destroy
   has_many :trips, through: :trip_trails
+
+  def total_trips
+    trips.all.count
+  end
 end
--- spec/features/visitors/user_visits_trail_show_spec.rb
+++ spec/features/visitors/user_visits_trail_show_spec.rb
@@ -12,4 +12,16 @@ describe 'A user visits the trail pages' do
       expect(page).to have_content(trail1.address)
     end
   end
+
+  describe 'shows total trips' do
+    it 'for each trail' do
+      trail1 = Trail.create!(length: 60, name: 'incline', address: '123')
+      trip1 = trail1.trips.create(name: 'bobs trail', start_date: Time.now, end_date: Time.now)
+      trip2 = trail1.trips.create!(name: 'lopsa', start_date: Time.now, end_date: Time.now)
+
+      visit trail_path(trail1)
+
+      expect(page).to have_content('Total Trips: 2')
+    end
+  end
 end
