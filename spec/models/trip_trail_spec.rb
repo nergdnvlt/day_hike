19c005479d409dc389a963913a66a8eeb978ca32
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-13 10:24:36 -0600

    First user story.

--- spec/models/trip_trail_spec.rb
+++ spec/models/trip_trail_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe TripTrail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Relationships' do
+    it { should belong_to(:trail) }
+    it { should belong_to(:trip) }
+  end
 end
